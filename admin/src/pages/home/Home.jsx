22382fd8210c0930c34ea2d3d57194a7fa7d1ccf
amrita-naik <naikamrita254@gmail.com>
2022-04-26 23:24:58 +0530

    admin

---
+++ admin/src/pages/home/Home.jsx
@@ -0,0 +1,42 @@
+import FeaturedInfo from '../../components/featuredInfo/FeaturedInfo'
+import './home.css'
+import { userData } from "../../dummyData";
+import Charts from '../../components/charts/Charts';
+import WidgetSm from '../../components/widgetSm/WidgetSm';
+import WidgetLg from '../../components/widgetLg/WidgetLg';
+import { useEffect, useMemo, useState } from 'react';
+import axios from 'axios'
+
+function Home() {
+    const MONTHS = useMemo(() => ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sept","Oct","Nov","Dec"])
+
+    const [userStats, setUserStats] = useState([])
+
+    useEffect(() => {
+        const getStats = async() => {
+            try {
+                const res = await axios.get('/users/stats', {headers: {token: "Bearer"}} )
+                const statsList = res.data.sort(function(a, b){
+                    return a._id - b._id
+                })
+                statsList.map(item => setUserStats(prev=>[...prev, {name:MONTHS[item._id-1], "New User": item.total}]))
+            }
+            catch (error) {
+                console.log(error)
+            }
+        } 
+        getStats();
+    }, [MONTHS])
+    return (
+        <div className='home'>
+            <FeaturedInfo />
+            <Charts data={userStats} title="User Analytics" grid dataKey="New User"/>
+            <div className="homeWidgets">
+                <WidgetSm />
+                <WidgetLg />
+            </div>
+        </div>
+    )
+}
+
+export default Home
